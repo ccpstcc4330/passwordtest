63bdf6c381647f6386c46ca6385ce03e4ea18185
Donna Co <chuhuilin228068@gmail.com>
2018-07-17 20:02:05 +0800

    Add new file

---
+++ password1.py
@@ -0,0 +1,14 @@
+password = 'jane12345'
+i = 3
+while i > 0:
+	pwd = input('請輸入密碼:')
+	if pwd == password:
+		print('登入成功!')
+		break
+	else:
+		i = i - 1
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有' , i , '機會')
+		else:
+			print('登入失敗!')
